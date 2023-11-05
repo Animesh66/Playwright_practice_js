97b67a1d2040e4d4e17d7114d4ade6f97a89f684
Animesh66 <er.animesh6@hotmail.com>
2023-11-05 21:51:11 +0100

    Added new ecomm test.

--- tests/DemoEcomm.spec.js
+++ tests/DemoEcomm.spec.js
@@ -6,4 +6,9 @@ test.only('Verify order placed with order id', async ({ browser }) => {
     await page.goto("https://rahulshettyacademy.com/client");
     await expect(page).toHaveTitle("Let's Shop");
     await page.getByRole('link', { name: 'Register' }).click();
+    await page.getByRole('textbox', { name: 'First Name' }).fill("Animesh");
+    await page.getByRole('textbox', { name: 'Last Name' }).fill("Mukherjee");
+    await page.getByRole('textbox', { name: 'Email' }).fill("anitest@email.com");
+    await page.getByRole('textbox', { name: 'Phone Number' }).fill("1122345833");
+    await page.pause();
 })
