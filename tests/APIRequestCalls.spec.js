6c93aa53b1fea819ee9d5fd4922572a29f197293
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2023-11-27 20:29:19 +0100

    Update APIRequestCalls.spec.js

--- tests/APIRequestCalls.spec.js
+++ tests/APIRequestCalls.spec.js
@@ -10,8 +10,9 @@ const createOrderPayload = {
             productOrderedId: "6262e95ae26b7e1a10e89bf0"
         }
     ]
-}
+};
 let token;
+let orderId;
 
 test.beforeAll('Login to application', async () => {
     const apiRequest = await request.newContext();
@@ -22,9 +23,19 @@ test.beforeAll('Login to application', async () => {
     const reponseJson = await loginResponse.json(); // convert response to JSON
     token = reponseJson.token; // Get the token from the reponse JSON
     console.log(token);
+    const orderResponse = await apiRequest.post(createOrderUrl,
+        {
+            data: createOrderPayload,
+            headers: {
+                'Authorization': token,
+                'Content-Type': 'application/json'
+            }
+        });
+    const orderResponseJson = await orderResponse.json();
+    orderId = orderResponseJson.orders[0]
 })
 
-test.only('Create Order', async ({browser}) => {
+test('Vaidate Home Page', async ({browser}) => {
     const context = await browser.newContext();
     const page = await browser.newPage();
     await page.addInitScript(async (value) => {  // Setting the value of token in local storage
@@ -32,4 +43,26 @@ test.only('Create Order', async ({browser}) => {
     }, token);
     await page.goto('https://rahulshettyacademy.com/client/');
     await expect(page.getByRole('button', { name: 'HOME'})).toBeVisible();
+})
+
+test.only('Create Orders', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await browser.newPage();
+    await page.addInitScript(async (value) => {  // Setting the value of token in local storage
+        window.localStorage.setItem('token', value);
+    }, token);
+    await page.goto('https://rahulshettyacademy.com/client/');
+    const orderRows = await page.locator("tbody tr");
+    const rowCount = await orderRows.count();
+    console.log(rowCount);
+    for (let i=0; i < rowCount; ++i){
+        const expectOrderId = await orderRows.nth(i).locator("th").textContent();
+        console.log(expectOrderId);
+        if (orderId.includes(expectOrderId)){
+            await orderRows.nth(i).getByRole('button', { name: 'View'}).click();
+            break;
+        }        
+    }
+    const actualOrderId = await page.locator('.col-text').textContent();
+    expect(orderId.includes(actualOrderId)).toBeTruthy();
 })
