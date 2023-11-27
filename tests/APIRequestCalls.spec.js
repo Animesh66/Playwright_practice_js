dcf48506d19b970b1610581496837460db41fc2e
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2023-11-27 19:49:58 +0100

    Update and rename NetworkInterrupt.spec.js to APIRequestCalls.spec.js

---
+++ tests/APIRequestCalls.spec.js
@@ -0,0 +1,13 @@
+const {test, expect, request} = require('@playwright/test');
+const exp = require('constants');
+const loginPayload = {userEmail: "anitest@email.com", userPassword: "Test12345678"}
+
+test.beforeAll('Login to application', async () => {
+    const apiRequest = await request.newContext();
+    const loginResponse = await apiRequest.post(
+        {url: 'https://rahulshettyacademy.com/api/ecom/auth/login'},
+        {data: loginPayload});
+    expect(loginResponse.ok()).toBeTruthy();  // Verify reponse code is 2XX
+    const reponseJson = loginResponse.json(); // convert response to JSON
+    const token = reponseJson.token; // Get the token from the reponse JSON
+})
--- tests/NetworkInterrupt.spec.js
+++
@@ -1,6 +0,0 @@
-const {test, expect, request} = require('@playwright/test')
-
-
-test.beforeAll('Login to application', async ({browser}) => {
-
-})
