08519e2d4386d8d0afff3f07b1b582ec1a404966
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2023-11-27 18:32:29 +0100

    Update HandleMultipleWindow.spec.js

--- tests/HandleMultipleWindow.spec.js
+++ tests/HandleMultipleWindow.spec.js
@@ -1,1 +1,14 @@
+const {test, expect, request} = require('@playwright/test')
+
+
+test.only('Login to application', async ({browser}) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.getByRole('link', {name: 'Free Access to InterviewQues/ResumeAssistance/Material'})
+    const [newPage] = await Promise.all(
+        [context.waitForEvent('page'), documentLink.click()]); // Wait for page and click on link. Race codition. 
+    const actualText = await newPage.locator('.red').textContent();
+    console.log(actualText);
+})  
 
