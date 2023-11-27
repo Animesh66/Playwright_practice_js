eb6c9cbe874b91a63bccb561c081bd55825493eb
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2023-11-27 20:12:40 +0100

    Update APIRequestCalls.spec.js

--- tests/APIRequestCalls.spec.js
+++ tests/APIRequestCalls.spec.js
@@ -1,11 +1,22 @@
 const {test, expect, request} = require('@playwright/test');
 const exp = require('constants');
+const loginUrl ='https://rahulshettyacademy.com/api/ecom/auth/login';
 const loginPayload = {userEmail: "anitest@email.com", userPassword: "Test12345678"};
+const createOrderUrl = 'https://rahulshettyacademy.com/api/ecom/order/create-order';
+const createOrderPayload = {
+    orders: [
+        {
+            country: "India",
+            productOrderedId: "6262e95ae26b7e1a10e89bf0"
+        }
+    ]
+}
 let token;
+
 test.beforeAll('Login to application', async () => {
     const apiRequest = await request.newContext();
     const loginResponse = await apiRequest.post(
-        'https://rahulshettyacademy.com/api/ecom/auth/login',
+        loginUrl,
         {data: loginPayload});
     expect(loginResponse.ok()).toBeTruthy();  // Verify reponse code is 2XX
     const reponseJson = await loginResponse.json(); // convert response to JSON
