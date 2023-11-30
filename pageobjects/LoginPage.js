f858882a3b4abd4afcbbcf482597a80df103767c
Animesh Mukherjee <37202195+Animesh66@users.noreply.github.com>
2023-11-30 19:13:01 +0100

    Create LoginPage.js

---
+++ pageobjects/LoginPage.js
@@ -0,0 +1,19 @@
+export class LoginPage {
+
+    constructor(page){
+        this.page = page;
+        this.userEmailBox = page.getByRole('textbox', { name: 'Email' });
+        this.userPasswordBox = page.getByPlaceholder('enter your passsword');
+        this.LoginPageButton = page.getByRole('button', { name: 'Login' });
+    }
+    async navigateUrl(baseUrl){
+        await this.page.goto(baseUrl);
+    }
+
+    async perfromLogin(userEmail, userPassword){
+        await this.userEmailBox.fill(userEmail);
+        await this.userPasswordBox.fill(userPassword);
+        await this.LoginPageButton.click();
+    }
+
+}
